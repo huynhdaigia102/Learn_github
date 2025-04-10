8857bce74daed74c8e0dc570f323643d0525ff91
VuNghiep <gno1channel@gmail.com>
2025-04-11 01:47:44 +0700

    them ham nhan

--- function.py
+++ function.py
@@ -2,10 +2,12 @@ def plus():
     print("9")
 
 #ham tru
-# def subtract():
-#     print('-1')
+def subtract():
+    print('-1')
 
-
+#ham nhan
+def nhan():
+    print("nhan")
 
 #ham chia
 def devide():
